16c3b5eb5ccda6931c78d44612296bbc10efa205
asimnisar2746 <asimnisar250@gmail.com>
2026-05-12 20:15:07 +0500

    fix refresh token cookies

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -63,14 +63,13 @@ export const loginUser = async (req, res) => {
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
     return res.status(200).json({
       message: "Login successful",
-
       accessToken,
 
       user: {
